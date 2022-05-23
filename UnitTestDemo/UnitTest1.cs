63a24afc7f4dead6a85f27bf3916bd6cf2b4d66a
邱禮晟 <charles0203@bankpro.com.tw>
2022-05-23 11:49:20 +0800

    XML 編碼問題：使用者輸入了一個特殊編碼【 \u0002】，經過XmlTextWriter物件提供的WriteElementString方法後，會被轉成【'&#x2;】

--- UnitTestDemo/UnitTest1.cs
+++ UnitTestDemo/UnitTest1.cs
@@ -1,14 +1,49 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using NUnit.Framework;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
 
 namespace UnitTestDemo
 {
-    [TestClass]
+
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+
+
+        //[TestCase(@"'嵐CASA(嵐 CASA)")]
+        [TestCase("")]
+        public void BuildFXML(string sMemo)
         {
+            Encoding oEncoding = Encoding.UTF8;
+            sMemo = "'嵐CASA(嵐 CASA)";
+
+            var oMemoryStream = new MemoryStream();
+            var oXmlTextWriter = new XmlTextWriter(oMemoryStream, oEncoding);
+            oXmlTextWriter.WriteStartDocument(false);
+            //====================================
+
+
+            oXmlTextWriter.WriteElementString("Memo", sMemo);
+
+
+            //====================================
+            oXmlTextWriter.WriteEndDocument();
+            oXmlTextWriter.Flush();
+
+
+            StreamReader reader = new StreamReader(oMemoryStream, oEncoding);
+            oMemoryStream.Seek(0, SeekOrigin.Begin);
+            string ss = reader.ReadToEnd();
+
+
+            Console.WriteLine(ss);
+
+            Assert.IsTrue(ss.Contains("&"));
         }
+
+
     }
 }
